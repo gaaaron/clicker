61bd155b13e4f00cf30431bef17438135dfe904d
gaaaron <garancsya@gmail.com>
2020-04-24 21:55:51 +0200

    Alert after stop

--- content_script.js
+++ content_script.js
@@ -10,12 +10,31 @@ function clickElement() {
     let intervalId = window.setInterval(function () {
 
         if (stopConditionIsMet()) {
+            beep(1000, 10);
             window.clearInterval(intervalId);
             return;
         }
 
         clickable.click();
     }, clickFreq * 1000);
+}
+
+function beep(duration = 200, repeat = 1) {
+    if (repeat == 0)
+        return;
+
+    let context = new AudioContext();
+    const oscillator = context.createOscillator();
+    const gain = context.createGain();
+    oscillator.connect(gain);
+    oscillator.frequency.value = 520;
+    oscillator.type = "square";
+    gain.connect(context.destination);
+    gain.gain.value = 1.01;
+    oscillator.start(context.currentTime);
+    oscillator.stop(context.currentTime + duration * 0.001);
+
+    setTimeout(_ => beep(duration, repeat-1), duration+500);
 }
 
 function stopConditionIsMet() {
